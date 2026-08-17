b72d328e277080fc138e029c73b02b5e493552c8
devpandey00 <diwakarpandeyitwa@gmail.com>
2026-08-17 16:11:18 +0530

    fix(shared): configure eslint project for tests

--- packages/shared/eslint.config.js
+++ packages/shared/eslint.config.js
@@ -1,4 +1,14 @@
 import nodeConfig from '@nexor/config/eslint/node';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...nodeConfig];
+export default [
+  ...nodeConfig,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.eslint.json'],
+      },
+    },
+  },
+];
