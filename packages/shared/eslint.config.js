b4255ecc2274273dcc6743a23f4b0fc081998a2e
devpandey00 <diwakarpandeyitwa@gmail.com>
2026-08-17 16:14:26 +0530

    fix(shared): rely on projectService for eslint ts files

--- packages/shared/eslint.config.js
+++ packages/shared/eslint.config.js
@@ -1,14 +1,4 @@
 import nodeConfig from '@nexor/config/eslint/node';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [
-  ...nodeConfig,
-  {
-    files: ['src/**/*.ts'],
-    languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.eslint.json'],
-      },
-    },
-  },
-];
+export default [...nodeConfig];
