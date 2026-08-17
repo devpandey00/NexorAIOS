22cedca3f45f48d3656b246ac73fd8e1c236a745
devpandey00 <diwakarpandeyitwa@gmail.com>
2026-08-17 16:15:25 +0530

    fix(shared): lint test files with dedicated eslint tsconfig

--- packages/shared/eslint.config.js
+++ packages/shared/eslint.config.js
@@ -1,4 +1,15 @@
 import nodeConfig from '@nexor/config/eslint/node';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...nodeConfig];
+export default [
+  ...nodeConfig,
+  {
+    files: ['src/**/*.test.ts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: false,
+        project: ['./tsconfig.eslint.json'],
+      },
+    },
+  },
+];
