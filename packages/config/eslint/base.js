8f8f41ad1f97bd45c17d71d870a23bd78adcea9d
devpandey00 <diwakarpandeyitwa@gmail.com>
2026-08-30 17:53:13 +0530

    fix(eslint): type-check repository test files safely

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -11,7 +11,9 @@ export default tseslint.config(
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        projectService: {
+          allowDefaultProject: ['**/*.test.ts', '**/*.test.tsx'],
+        },
       },
     },
     rules: {
